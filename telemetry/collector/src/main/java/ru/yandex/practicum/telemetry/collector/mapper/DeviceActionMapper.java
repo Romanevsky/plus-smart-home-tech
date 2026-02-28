a4d4b259af91f6c6bcb12d840a0ca33f0fa44390
Романевский <V9139493364@yandex.ru>
2026-02-28 21:23:34 +0700

    Правка замечаний

--- telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/mapper/DeviceActionMapper.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/telemetry/collector/mapper/DeviceActionMapper.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.telemetry.collector.mapper;
 
+import lombok.experimental.UtilityClass;
 import ru.yandex.practicum.kafka.telemetry.event.ActionTypeAvro;
 import ru.yandex.practicum.kafka.telemetry.event.DeviceActionAvro;
 import ru.yandex.practicum.telemetry.collector.model.hub.DeviceAction;
 
+@UtilityClass
 public class DeviceActionMapper {
     public static DeviceActionAvro modelToAvro(DeviceAction action) {
         return DeviceActionAvro.newBuilder()
