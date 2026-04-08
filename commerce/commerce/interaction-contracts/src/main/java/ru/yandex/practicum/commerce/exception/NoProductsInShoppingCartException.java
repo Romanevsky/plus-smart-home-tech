9c02ebf52adc9f6f7734b058d2c067d9d6e5a7bf
Романевский <V9139493364@yandex.ru>
2026-04-08 23:52:03 +0700

    правки по замечанию

--- commerce/commerce/interaction-contracts/src/main/java/ru/yandex/practicum/commerce/exception/NoProductsInShoppingCartException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.commerce.exception;
-
-public class NoProductsInShoppingCartException extends RuntimeException {
-    public NoProductsInShoppingCartException(String message) {
-        super(message);
-    }
-}
--- commerce/commerce/interaction-contracts/src/main/java/ru/yandex/practicum/commerce/exception/SpecifiedProductAlreadyInWarehouseException.java
+++
@@ -1,7 +0,0 @@
-package ru.yandex.practicum.commerce.exception;
-
-public class SpecifiedProductAlreadyInWarehouseException extends RuntimeException {
-    public SpecifiedProductAlreadyInWarehouseException(String message) {
-        super(message);
-    }
-}
